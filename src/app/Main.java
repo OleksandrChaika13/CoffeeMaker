f8655e3ce1aec05dfbbc6d4ed90e7388313aff38
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-20 11:50:24 +0300

    Creating "Main" class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,19 @@
 package app;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+        coffeeMachine.displayMenu();
+        int choice = coffeeMachine.chooseDrink();
+        Coffee coffee = coffeeMachine.createCoffee(choice);
+
+        if (coffee != null) {
+            coffeeMachine.makeCoffee(coffee);
+        } else {
+            System.out.println("Invalid choice!");
+        }
+    }
 }
