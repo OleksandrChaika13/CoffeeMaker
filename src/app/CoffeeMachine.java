37531554bc6b549d390bef8f1dd9a39f57cfa062
OleksandrChaika13 <axelrod1347@gmail.com>
2023-08-20 11:49:38 +0300

    Creating "CoffeeMachine" class

--- src/app/CoffeeMachine.java
+++ src/app/CoffeeMachine.java
@@ -1,4 +1,55 @@
 package app;
 
-public class CoffeeMachine {
+import java.util.Scanner;
+
+public class CoffeeMachine implements Machine {
+
+    Scanner scanner;
+
+    public CoffeeMachine() {
+        scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public void displayMenu() {
+        System.out.println("""
+                Choose a drink:
+                1 - Espresso
+                2 - Cappuccino
+                3 - Latte
+                """);
+    }
+
+    @Override
+    public int chooseDrink() {
+        int choice;
+        while (true) {
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+                if (choice >= 1 && choice <= 3) {
+                    break;
+                } else {
+                    System.out.println("Invalid choice! Please select a valid drink.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input! Please enter a valid number.");
+            }
+        }
+        return choice;
+    }
+
+    @Override
+    public void makeCoffee(Coffee coffee) {
+        coffee.make();
+        System.out.println("Your " + coffee.getName() + " is ready. Enjoy :)");
+    }
+
+    public Coffee createCoffee(int choice) {
+        return switch (choice) {
+            case 1 -> new Espresso();
+            case 2 -> new Cappuccino();
+            case 3 -> new Latte();
+            default -> null;
+        };
+    }
 }
